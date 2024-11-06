609f014a256dfdf455daf4fdb15548e9d1d1ca9d
NeroBoyka <russelldossouyovo@gmail.com>
2024-11-06 14:56:07 +0100

    Ajout fonction AfficherCouleur

--- Motus/Program.cs
+++ Motus/Program.cs
@@ -6,5 +6,13 @@
         {
             Console.WriteLine("Hello, World!");
         }
+
+        static void AfficherCouleur(String texte, ConsoleColor couleur)
+        {
+            Console.ForegroundColor = couleur;
+            Console.Write(texte);
+
+            Console.ForegroundColor = ConsoleColor.White;
+        }
     }
 }
