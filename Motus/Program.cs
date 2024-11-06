9db56edabe83b7446b4e2835c170fd0928f6d6b1
NeroBoyka <russelldossouyovo@gmail.com>
2024-11-07 00:25:05 +0100

    rename mot.txt to mots.txt + implement the main function + small fix

--- Motus/Program.cs
+++ Motus/Program.cs
@@ -6,7 +6,62 @@ namespace Motus
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine(GetMot(5));
+            List<string> listeMots = ChargerMots("mots.txt");
+            bool replay = true;
+            string replayAnswer = "";
+
+            while (replay)
+            {
+                Console.WriteLine("Bienvenue dans Motus\n");
+                string motCache = ChoisirMot(listeMots).ToUpper();
+                int tailleMotCache = motCache.Length;
+
+                //Console.WriteLine(motCache); // pour test
+
+                Console.WriteLine($"Le mot caché contient {tailleMotCache} caractères et commence par {motCache[0]}\n");
+
+                for (int i = 6; i >= 1; i--)
+                {
+                    Console.WriteLine($"Il vous reste {i} coups a jouer");
+                    string motSaisie = GetMot(tailleMotCache);
+                    
+                    TestMot(motCache, motSaisie);
+
+                    if (motCache == motSaisie)
+                    {
+                        Console.WriteLine($"\nBravo vous avez gagné, le mot caché était: {motCache}\n");
+                        break;
+                    }
+                    else if(motCache != motSaisie)
+                    {
+                        if (i == 1)
+                        {
+                            Console.WriteLine($"Désole vous avez perdu ! Le mot caché était : {motCache}");
+                        }
+                    }
+
+                    Console.WriteLine();
+                }
+
+                // boucle pour verifier que la replayAnswer donnee est valable
+                while (true)
+                {
+                    Console.WriteLine("Voulez vous rejouer? (o / n)");
+                    replayAnswer = Console.ReadLine().ToLower();
+                    if(replayAnswer == "oui" || replayAnswer == "o")
+                    {
+                        replay = true;
+                        Console.WriteLine();
+                        break;
+                    }
+                    else if (replayAnswer == "non" || replayAnswer == "n")
+                    {
+                        replay = false;
+                        Console.WriteLine();
+                        break;
+                    }
+                }
+            }
         }
 
         static void AfficherCouleur(char texte, ConsoleColor couleur)
@@ -23,8 +78,8 @@ namespace Motus
 
             while (inputWord.Length != lenMot)
             {
-                Console.WriteLine("Entrez un mot de " + lenMot + " caracteres: ");
-                inputWord = Console.ReadLine().ToLower();
+                Console.WriteLine("Entrez un mot de " + lenMot + " caractères: ");
+                inputWord = Console.ReadLine().ToUpper();
             }
 
             return inputWord;
@@ -53,6 +108,8 @@ namespace Motus
 
         static void TestMot(String motCache, String motSaisie)
         {
+            Console.Write ("Votre mot contient: ");
+
             for (int i = 0; i < motCache.Length; i++)
             {
                 if (motSaisie[i] == motCache[i])
